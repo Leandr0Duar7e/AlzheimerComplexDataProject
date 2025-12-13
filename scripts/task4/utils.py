bb84fadd6dd6b4b45d0a0ef07dd267c7a97c2390
Leandr0Duar7e <lean_duart@hotmail.com>
2025-12-13 21:40:59 +0100

    Project finalized

---
+++ scripts/task4/utils.py
@@ -0,0 +1,25 @@
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class MRIDataset(Dataset):
+    def __init__(self, dataframe, transform=None):
+        self.dataframe = dataframe.reset_index(drop=True)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, idx):
+        row = self.dataframe.iloc[idx]
+        img_path = row["path"]
+        label = row["label"]
+
+        # Load image and convert to RGB
+        image = Image.open(img_path).convert("RGB")
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, torch.tensor(label, dtype=torch.long)
